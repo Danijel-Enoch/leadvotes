085f497ee2d787f059b4e43570af24c0fc1074ef
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-02 03:51:52 +0100

    feat: create and complete About component

--- app/components/Homepage.tsx
+++ app/components/Homepage.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
+import About from './About'
 
 const Homepage = () => {
   return (
@@ -10,6 +11,7 @@ const Homepage = () => {
           <p className='text-[19px] font-medium leading-[28.5px] my-[40px] w-[45%] mx-auto '>A beauty pageantry is a competition that has traditionally focused on judging and ranking the physical...</p>
           <button className='rounded-[40px] bg-white w-[172px] h-[57px] text-[#202020] text-[18px] font-bold leading-[27px]  '>Create poll</button>
         </section>
+        <About />
     </main>
   )
 }
