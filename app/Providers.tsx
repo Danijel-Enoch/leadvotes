a2b5b05283ab5c6a006d45b4ed0dcec68859e385
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-04 09:16:05 +0100

    feat: implement connect wallet button

---
+++ app/Providers.tsx
@@ -0,0 +1,87 @@
+"use client";
+import { useConfig } from "wagmi";
+import "@rainbow-me/rainbowkit/styles.css";
+import {
+	getDefaultConfig,
+	RainbowKitProvider,
+	Theme
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const myCustomTheme: Theme = {
+	blurs: {
+		modalOverlay: "..."
+	},
+	colors: {
+		accentColor: "",
+		accentColorForeground: "white",
+		actionButtonBorder: "black",
+		actionButtonBorderMobile: "...",
+		actionButtonSecondaryBackground: "#1b5cfe",
+		closeButton: "...",
+		closeButtonBackground: "...",
+		connectButtonBackground: "#34C055",
+		generalBorder: "#34C055",
+		connectButtonText: "#0A1517",
+		modalBackground: "white",
+		connectButtonBackgroundError: "...",
+		connectButtonInnerBackground: "...",
+		connectButtonTextError: "...",
+		connectionIndicator: "...",
+		downloadBottomCardBackground: "...",
+		downloadTopCardBackground: "...",
+		error: "...",
+		generalBorderDim: "...",
+		menuItemBackground: "...",
+		modalBackdrop: "...",
+		modalBorder: "...",
+		modalText: "#1E1E1E",
+		modalTextDim: "#1E1E1E",
+		modalTextSecondary: "#1E1E1E",
+		profileAction: "...",
+		profileActionHover: "...",
+		profileForeground: "...",
+		selectedOptionBorder: "...",
+		standby: "..."
+	},
+	radii: {
+		actionButton: "9999px",
+		connectButton: "12px",
+		menuButton: "12px",
+		modal: "24px",
+		modalMobile: "28px"
+	},
+	fonts: {
+		body: "'DM Sans', sans-serif"
+	},
+	shadows: {
+		connectButton: "...",
+		dialog: "...",
+		profileDetailsAction: "...",
+		selectedOption: "...",
+		selectedWallet: "...",
+		walletLogo: "..."
+	}
+};
+export default function Providers({ children }: any) {
+	const queryClient = new QueryClient();
+
+	const config = getDefaultConfig({
+		appName: "LEADVOTE",
+		projectId: "YOUR_PROJECT_ID",
+		chains: [mainnet],
+		ssr: true // If your dApp uses server side rendering (SSR)
+	});
+
+	return (
+		<WagmiProvider config={config}>
+			<QueryClientProvider client={queryClient}>
+				<RainbowKitProvider coolMode theme={myCustomTheme}>
+						{children}
+				</RainbowKitProvider>
+			</QueryClientProvider>
+		</WagmiProvider>
+	);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import Providers from "./Providers";
 
 const poppins = Poppins({  weight: ['400', '700'],
 style: ['normal'],
@@ -19,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}><Providers>{children} </Providers>{" "}</body>
     </html>
   )
 }
