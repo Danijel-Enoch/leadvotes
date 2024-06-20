6054cb372261aa297f6904534cfe95a5678cbbdb
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-20 09:00:03 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 "use client"
-// import React, { useEffect } from "react"
+import React, { useEffect } from "react"
 import Homepage from './components/Homepage'
-// import Aos from "aos"
-// import "aos/dist/aos.css"; 
+import Aos from "aos"
+import "aos/dist/aos.css"; 
 
 
 export default function Home() {
 
 
-  // useEffect(() => {
-  //   Aos.init({
-  //      offset: 0,
-  //      duration: 1200,
-  //      easing: "ease",
-  //      delay: 0,
-  //   })
-  // }, [])
+  useEffect(() => {
+    Aos.init({
+       offset: 0,
+       duration: 1200,
+       easing: "ease",
+       delay: 0,
+    })
+  }, [])
 
   return (
     <main className='flex justify-center  '>
