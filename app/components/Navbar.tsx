30b0e9187f81a1550e326068e6cafc1345cd7ec4
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-06-21 23:25:44 +0100

    feat: modify sign up page

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,14 +11,18 @@ const Navbar = () => {
 			<section className="lg:w-[80%] w-full ">
 				<div className="flex justify-between bg-[#1E1E1E6E] rounded-[66px] border-[2px] border-[#373737] h-[60px] lg:h-[82px] ">
 					<Link href="/" className="my-auto ">
-						<h1 className="ml-[20px] lg:ml-[30px] my-auto text-[20px] lg:text-[25px] font-medium ">
+						<h1 className="ml-[20px] lg:ml-[30px] my-auto text-[18px] lg:text-[25px] font-medium ">
 							<span className="text-[#1b5cfe]">Lead</span>Votes
 						</h1>
 					</Link>
-
-					<button className="bg-[#1B5CFE] hover:bg-[#1948b5] rounded-[89px] h-[40px] lg:h-[53px] my-auto text-[13px] lg:text-[15px] leading-[22.5px] mr-[10px] lg:mr-[17px] font-medium px-[8px] lg:px-[25px] ">
-						<ConnectButton />
-					</button>
+					<div className="flex">
+						<Link href='/sign-up' className="my-auto">
+						<button className="bg-[#1B5CFE] font-semibold hover:bg-[#1948b5] rounded-[89px] h-[40px] lg:h-[53px] lg:w-[105px] w-[70px] mr-[7px] my-auto text-[12px] lg:text-[15px] leading-[22.5px]">Sign Up</button>
+						</Link>
+						<button className="bg-[#1B5CFE] hover:bg-[#1948b5] rounded-[89px] h-[40px] lg:h-[53px] my-auto text-[12px] lg:text-[15px] leading-[22.5px] mr-[10px] lg:mr-[17px] font-medium px-[0px] lg:px-[25px] ">
+							<ConnectButton />
+						</button>
+					</div>
 				</div>
 			</section>
 		</main>
--- app/sign-up/page.jsx
+++ app/sign-up/page.jsx
@@ -1,11 +1,13 @@
 "use client";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from 'next/image'
+import box from '../assets/box.png'
 import React, { useState } from "react";
 import { useAccount, useSignMessage } from "wagmi";
 import { useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { stringify } from "viem";
+import Navbar from "../components/Navbar";
 
 export default function SignUp() {
 	const [studentData, setStudentData] = useState({});
@@ -26,91 +28,107 @@ export default function SignUp() {
 		isLoading ? toast.info("Please wait...") : null;
 	}, [isConnected, address, studentData]);
 	return (
-		<div>
-			<ConnectButton />
-			<h1>Sign Up</h1>
+		<main className='flex justify-center  '>
+			<section className='lg:max-w-[1400px] w-[95%] lg:mx-[0px] mx-auto lg:w-full' >
+				<Navbar />
 
-			<div>
-				<label>Email (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({
-							...studentData,
-							email: e.target.value
-						})
-					}
-					type="text"
-				/>
-			</div>
+				<div className='lg:w-[80%] w-full mt-[25px] mx-auto '>
+					<Image src={box} alt='tree-img' className='w-full rounded-[40px] h-[100px] lg:h-[266px] ' data-aos="zoom-in" />
+					<h1 className='lg:text-[55px] text-[30px] leading-[50px] lg:leading-[82.5px] mt-[40px] lg:mt-[60px] text-center mb-[15px] lg:mb-[30px]' data-aos="zoom-out">Sign Up</h1>
+					<form className="w-fit mx-auto ">
+						<div  className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Email (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({
+										...studentData,
+										email: e.target.value
+									})
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
 
-			<div>
-				<label>Matric Number (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({
-							...studentData,
-							matric: e.target.value
-						})
-					}
-					type="text"
-				/>
-			</div>
+						<div className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Matric Number (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({
+										...studentData,
+										matric: e.target.value
+									})
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
 
-			<div>
-				<label>Department (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({ ...studentData, dept: e.target.value })
-					}
-					type="text"
-				/>
-			</div>
-			<div>
-				<label>Faculty (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({ ...studentData, fac: e.target.value })
-					}
-					type="text"
-				/>
-			</div>
-			<div>
-				<label>Level (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({
-							...studentData,
-							level: e.target.value
-						})
-					}
-					type="text"
-				/>
-			</div>
-			<div>
-				<label>Name (Required)</label>
-				<input
-					onChange={(e) =>
-						setStudentData({ ...studentData, name: e.target.value })
-					}
-					type="text"
-				/>
-			</div>
-			<button
-				onClick={() => {
-					if (
-						studentData.matric &&
-						studentData.name &&
-						studentData.falcuty
-					) {
-						signMessage({ message: stringify(studentData) });
-					} else {
-						toast.error("All fields are required");
-					}
-				}}
-			>
-				Sign Up
-			</button>
-			<ToastContainer />
-		</div>
+						<div className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Department (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({ ...studentData, dept: e.target.value })
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
+						<div className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Faculty (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({ ...studentData, fac: e.target.value })
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
+						<div className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Level (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({
+										...studentData,
+										level: e.target.value
+									})
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
+						<div className="flex flex-col ">
+							<label className='text-[20px] lg:text-[20px] font-medium leading-[33px] mb-[10px]  mt-[10px]  '>Name (Required)</label>
+							<input
+								onChange={(e) =>
+									setStudentData({ ...studentData, name: e.target.value })
+								}
+								type="text"
+								className='bg-[#151515] rounded-[12px] border-[2px] border-[#939393] mb-[20px] lg:mb-[30px] lg:w-[510px] h-[58px]  lg:h-[58px]  outline outline-none bg-[#151515] text-[#00000066] text-white pl-[15px] rounded-[12px] '
+							/>
+						</div>
+						<div className="w-fit mx-auto mt-[15px] ">
+						<button
+								className='font-semibold text-[20px] lg:text-[22px] mb-[60px] text-white bg-[#1B5CFE] lg:w-[220px] w-[180px] h-[58px] lg:h-[60px] rounded-[10px] hover:bg-[#1948b5] ' data-aos="zoom-in"
+							onClick={() => {
+								if (
+									studentData.matric &&
+									studentData.name &&
+									studentData.falcuty
+								) {
+									signMessage({ message: stringify(studentData) });
+								} else {
+									toast.error("All fields are required");
+								}
+							}}
+						>
+							Sign Up
+						</button>
+						</div>
+						<ToastContainer />
+					</form>
+				</div>
+			</section>
+		</main>
 	);
 }
